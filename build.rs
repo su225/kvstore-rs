4c93b1cce33a0b1094798a47b56c1444348238a4
Suchith J N <suchithjn22@gmail.com>
2024-07-12 21:11:20 +0200

    rustfmt

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
 fn main() {
-    println!("cargo:rustc-env=CARGO_PKG_AUTHORS={}", env!("CARGO_PKG_AUTHORS"));
-    println!("cargo:rustc-env=CARGO_PKG_DESCRIPTION={}", env!("CARGO_PKG_DESCRIPTION"));
-    println!("cargo:rustc-env=CARGO_PKG_VERSION={}", env!("CARGO_PKG_VERSION"));
+    println!(
+        "cargo:rustc-env=CARGO_PKG_AUTHORS={}",
+        env!("CARGO_PKG_AUTHORS")
+    );
+    println!(
+        "cargo:rustc-env=CARGO_PKG_DESCRIPTION={}",
+        env!("CARGO_PKG_DESCRIPTION")
+    );
+    println!(
+        "cargo:rustc-env=CARGO_PKG_VERSION={}",
+        env!("CARGO_PKG_VERSION")
+    );
 }
--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,5 +1,5 @@
-use std::process::exit;
 use clap::{arg, Command};
+use std::process::exit;
 
 fn main() {
     let matches = Command::new("kvstore-rs")
@@ -15,7 +15,7 @@ fn main() {
         .subcommand(
             Command::new("get")
                 .about("Get the string value of a given string key")
-                .arg(arg!(<KEY> "the key to get"))
+                .arg(arg!(<KEY> "the key to get")),
         )
         .subcommand(
             Command::new("rm")
@@ -30,20 +30,20 @@ fn main() {
             let _value = sub_m.get_one::<String>("VALUE").unwrap();
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         Some(("get", sub_m)) => {
             let _key = sub_m.get_one::<String>("KEY").unwrap();
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         Some(("rm", sub_m)) => {
             let _key = sub_m.get_one::<String>("KEY").unwrap();
             eprintln!("unimplemented");
             exit(1);
-        },
+        }
         _ => {
             eprintln!("Use {} -h for help", env!("CARGO_PKG_NAME"));
             exit(1);
-        },
+        }
     }
 }
